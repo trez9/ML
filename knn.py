0994103e5ffc88ccd83a86bb0160e5224abc0df6
Tara Rezvani <tara.rezvani@berkeley.edu>
2016-09-22 10:08:16 -0700

    initial commit

---
+++ knn.py
@@ -0,0 +1,10 @@
+# sklearn.neighbors provides functionality for unsupervised and supervised neighbors-based learning methods
+# supervised learning has 2 flavors
+# 	- classification for data with discrete labels
+#   - regression for data with continuous labels
+# 
+# Basic principal - find a predefined number of training samples closest in distance to the new point
+# & predict the label from these
+
+# Classification libs: KNeighborsClassifier, RadiusNeighborsClassifier
+
